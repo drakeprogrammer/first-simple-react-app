fdcd6f7919eee366d0f0dc8ff5d2b29a3465919b
drakeprogrammer <drakeprogrammers@gmail.com>
2020-01-17 23:35:49 +0100

    04-01 static component of appllication

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <div className="countdown">
+            <strong>śniadanie </strong> - 7:00
+        </div>
+        <div className="countdown">
+            <strong>obiad </strong> - 15:00
+        </div>
+    </div>
+);
+
+export default App;
