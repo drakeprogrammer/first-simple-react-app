10dae7ca0eb1f1d560e63226f281c32c16de2826
drakeprogrammer <drakeprogrammers@gmail.com>
2020-01-18 00:03:50 +0100

    04-05 create state component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 import Countdown from "./Countdown";
 
-const App = () => (
-    <div>
-        <Countdown name="śniadanie" time="7:00"/>
-        <Countdown name="obiad" time="15:00"/>
-    </div>
-);
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            events: [
+                {id:0, name: "śniadanie", time: "7:00"},
+                {id:1, name: "obiad", time: "15:00"}
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Countdown name="śniadanie" time="7:00"/>
+                <Countdown name="obiad" time="15:00"/>
+            </div>
+        )
+    }
+}
+
 
 export default App;
