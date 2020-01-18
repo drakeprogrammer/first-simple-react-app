e88057ad7a3b9454dd6d0158b3c347be6981cb46
drakeprogrammer <drakeprogrammers@gmail.com>
2020-01-18 22:43:50 +0100

    05-02-component-to-edit-html-event-part2

--- src/EditEvent.jsx
+++ src/EditEvent.jsx
@@ -7,6 +7,17 @@ const EditEnv = () => {
                 <label htmlFor="name">name</label>
                 <input id="name" type="text" name="name" />
             </div>
+
+            <div className="edit-event__input-group">
+                <label htmlFor="hour">hour</label>
+                <input id="hour" type="tel" name="hour" />
+            </div>
+            <div className="edit-event__input-group">
+                <label htmlFor="minute">minute</label>
+                <input id="minute" type="tel" name="minute" />
+            </div>
+            <button>OK</button>
+            <button>Cancel</button>
         </div>
     )
 };
